1b65246279c10558010c298d634069d240d34f66
ZHANG Cheng <czhang.oss@gmail.com>
2023-03-06 03:37:02 +0000

    add rollbar

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -28,6 +28,10 @@ import watchtower
 import logging
 from time import strftime
 
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
 
 # Configuring Logger to Use CloudWatch
 LOGGER = logging.getLogger(__name__)
@@ -159,6 +163,28 @@ def data_activities_reply(activity_uuid):
   else:
     return model['data'], 200
   return
+
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello Rollbar!', 'warning')
+    return "Rollbar Test!"
+
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
 @app.after_request
 def after_request(response):
